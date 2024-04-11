6f4ad5e3a0519ff7706137ee6f37ddf28713fc51
Paurush Choudhary <paurushchoudhary@Paurushs-MacBook-Pro.local>
2024-04-11 14:36:40 +0530

    Initial commit

---
+++ src/main/java/com/flight/baggagehandlerservice/BaggageHandlerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flight.baggagehandlerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaggageHandlerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaggageHandlerServiceApplication.class, args);
+	}
+
+}
