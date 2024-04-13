05b5d2f926e9dee71484598fee119cac4a992753
Paurush Choudhary <paurushchoudhary@Paurushs-MacBook-Pro.local>
2024-04-13 13:19:18 +0530

    BHS backend logic

--- src/main/java/com/flight/baggagehandlerservice/BaggageHandlerServiceApplication.java
+++ src/main/java/com/flight/baggagehandlerservice/BaggageHandlerServiceApplication.java
@@ -2,8 +2,10 @@ package com.flight.baggagehandlerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BaggageHandlerServiceApplication {
 
 	public static void main(String[] args) {
